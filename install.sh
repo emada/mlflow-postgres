fc7283df3f170b0a10656da2c1218830d1a73b96
Emerson <bmi.machado@gmail.com>
2023-06-12 10:28:20 +1000

    Fixed systemd issue. Added restart option

--- install.sh
+++ install.sh
@@ -7,13 +7,14 @@ cp mlflow.service /etc/systemd/system/mlflow.service
 
 rm -rf ${INSTALL_DIR}
 mkdir -p ${INSTALL_DIR}
-cp README.md ${INSTALL_DIR}
 cp .env ${INSTALL_DIR}
 cp docker-compose.yml ${INSTALL_DIR}
-cp Dockerfile ${INSTALL_DIR}
+cp mlflow.Dockerfile ${INSTALL_DIR}
+cp README.md ${INSTALL_DIR}
 cp requirements.txt ${INSTALL_DIR}
 
 systemctl daemon-reload
 
-echo "[ OK ] MLflow service successfully installed!"
-echo "Use 'systemctl start mlflow' to start the MLflow server"
+echo "MLflow service successfully installed!"
+echo "You can now delete this installation folder including the .env file."
+echo "Use 'systemctl start mlflow' to start MLflow"
